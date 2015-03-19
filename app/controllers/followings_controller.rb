fb2b844711a8f588f513b037b579ba53b378b7be
fabianVeliz <velizfabianhoracio@gmail.com>
2015-03-19 12:28:46 -0300

    Refactoring followings_controller and users_controller

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -2,20 +2,21 @@ class FollowingsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    user_name = params[:user_name]
-    user_following = User.find_by(user_name: user_name)
+    slug = params[:slug]
+    user_following = User.friendly.find(slug)
 
     following = current_user.followings.build( user_following_id: user_following.id)
 
     if following.save
-      redirect_to user_profile_path(user_name), notice: 'You follow a user'
+      redirect_to user_profile_path(slug), notice: 'You follow a user'
     else
       redirect_to :back
     end
   end
 
   def destroy
-    following = current_user.followings.where(user_following_id: params[:id]).first
+    user_id = params[:id]
+    following = current_user.followings.where(user_following_id: user_id).first
     following.destroy
     redirect_to :back
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,4 @@ class UsersController < ApplicationController
 
   def index; end
   def profile; end
-
 end
