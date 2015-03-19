aa80cbe762f87e5c171b4c6414e1d20f9a2a0cc2
fabianVeliz <velizfabianhoracio@gmail.com>
2015-03-19 11:52:29 -0300

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-User.create(email: 'fabian@insignia4u.com', password: 'asdasdasd', user_name: 'fabianVeliz')
-User.create(email: 'user@user.com', password: 'asdasdasd', user_name: 'user')
-User.create(email: 'user1@user.com', password: 'asdasdasd', user_name: 'user1')
-User.create(email: 'user2@user.com', password: 'asdasdasd', user_name: 'user2')
-User.create(email: 'user3@user.com', password: 'asdasdasd', user_name: 'user3')
-User.create(email: 'user4@user.com', password: 'asdasdasd', user_name: 'user4')
-User.create(email: 'user5@user.com', password: 'asdasdasd', user_name: 'user5')
-
-puts '-> Users have been created'
+5.times do |i|
+  User.create(email: "user#{i}@users.com", password: "asdasdasd", user_name: "user #{i}")
+  puts "-> User user#{i}@users.com created"
+end
